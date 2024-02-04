a2366e77382b4e4b287f17b40bc68ad2f0404653
Somanith <SomanithSounexample.com>
2024-02-04 19:43:41 +0700

    2

--- bmi_calc.py
+++ bmi_calc.py
@@ -10,21 +10,21 @@ Reminders:
 
 
 def bmi_check(weight, height):
-    if not (isinstance(weight, float) and isinstance(height, float)):
-        return "invalid input"
-    
-    if (weight > 635) and (height > 2.27):
-        print("unrealistic information")
-    else:
-        bmi_calculate =  weight/(height**2)
-        if bmi_calculate < 18.5:
-            return "underweight by ", bmi_calculate 
-        elif bmi_calculate <= 24.9:
-            return  "normal by ", bmi_calculate
-        elif bmi_calculate <= 29.9:
-            return "overweight ", bmi_calculate
-        elif bmi_calculate <= 39.9:
-            return "obese by ", bmi_calculate
-        elif bmi_calculate <= 40:
-            return  "extrmely obese by ", bmi_calculate
+   
+        if not isinstance(weight, (int, float)) or not isinstance(height, (int, float)):
+                return "invalid input", 1
 
+        if weight <= 635 and height <= 2.72:
+                result = round(weight/height ** 2, 2)
+                if result < 18.5:
+                        return "underweight", result
+                elif result >= 18.5 and result <= 24.9:
+                        return "normal", result
+                elif result >= 25 and result <= 29.9:
+                        return "overweight", result
+                elif result >= 30 and result <= 34.9:
+                        return "obese", result
+                else:
+                        return "extremely obese", result
+        else:
+                return "unrealistic information", 2
