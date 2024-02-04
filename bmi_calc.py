49a81d795fa8f30ef2b0b61b3696961b7e0026bc
Somanith <SomanithSounexample.com>
2024-02-04 10:23:25 +0700

    1

--- bmi_calc.py
+++ bmi_calc.py
@@ -7,6 +7,24 @@ Reminders:
 - calculate bmi before returning outputs 
 - add error checking for invalid input and unrealistic information
 '''
+
+
 def bmi_check(weight, height):
+    if not (isinstance(weight, float) and isinstance(height, float)):
+        return "invalid input"
     
+    if (weight > 635) and (height > 2.27):
+        print("unrealistic information")
+    else:
+        bmi_calculate =  weight/(height**2)
+        if bmi_calculate < 18.5:
+            return "underweight by ", bmi_calculate 
+        elif 18.5 <= bmi_calculate <= 24.9:
+            return  "normal by ", bmi_calculate
+        elif 25 <= bmi_calculate <= 29.9:
+            return "overweight ", bmi_calculate
+        elif 30 <= bmi_calculate <= 39.9:
+            return "obese by ", bmi_calculate
+        elif bmi_calculate <= 40:
+            return  "extrmely obese by ", bmi_calculate
 
--- main.py
+++ main.py
@@ -3,5 +3,19 @@ from bmi_calc import bmi_check
 # Call the bmi_calc function and add in a user interface for the bmi calculator (ex. welcome message, instructions, etc.)
 
 # Add your code here
+while True:
     name = input("What is your name? : ")
+    weight = input("How much do you weight? (in kg): ")
+    height = input("How tall are you? (in m): ")
+
+
+    print(f"Hello {name} You are {bmi_check(weight, height)} BMI")
+
+    con = input("Do you want to continue? Y/N: ")
+    if con.lower() == "n":
+        print("Thank you for using our app")
+        print("Hope you have a great day, dear!! <3")
+        exit()
+    else:
+        continue
     
